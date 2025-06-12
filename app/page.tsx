f2d9b58f2deebdc23cceb35c471b917f1314607e
Maelys Guardini <louguardini@gmail.com>
2025-06-13 01:15:43 +0200

    first commit front tnw, basic config and first idea of login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,52 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState<string>("");
+  const [nickname, setNickname] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="grid grid-rows-[110px_1fr_70px] items-center justify-items-center min-h-screen ">
+      <section className="bg-slate-800 w-full  flex items-center justify-center row-start-1 h-full">
+        <p> header</p>
+      </section>
+      <section className="grid grid-cols-3 grid-rows-6 w-full bg-blue-200  row-start-2 h-full">
+        <div className="flex flex-col bg-white row-start-2 col-start-2 row-end-5 text-black shadow rounded-3xl items-center justify-between">
+          <h1 className="pb-3 pt-10">Bienvenue !</h1>
+          <div className="flex flex-row gap-2 w-[80%] items-center justify-between">
+            <input
+              placeholder="name"
+              className="border-2 rounded-3xl p-3 border-neutral-300 w-[50%]"
+            ></input>
+            <input
+              placeholder="nickname"
+              className="border-2 rounded-3xl p-3 border-neutral-300 w-[50%]"
+            ></input>
+          </div>
+          <input
+            placeholder="email"
+            className="border-2 rounded-3xl p-3 border-neutral-300 mt-1 w-[80%]"
+          ></input>
+          <div className="grid grid-cols-2 w-full row-start-2 h-16">
+            <button
+              onClick={(): void =>
+                console.log("L'utilisateur veut s'inscrire.")
+              }
+              className="flex col-start-1 bg-blue-500 w-full justify-center items-center rounded-bl-3xl font-bold text-white "
+            >
+              S'inscrire
+            </button>
+            <p className="flex col-start-2 bg-slate-800 w-full justify-center text-white items-center rounded-br-3xl font-bold">
+              Se connecter
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+      <section className="flex w-full bg-neutral-800  items-center justify-center row-start-3 h-full">
+        <p>footer</p>
+      </section>
+    </main>
   );
 }
