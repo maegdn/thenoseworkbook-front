185552bc7efa20c4770b248ea778c6b82e6b1e52
Maelys Guardini <louguardini@gmail.com>
2025-06-14 02:31:47 +0200

    small changes and added password input + registration route

--- app/page.tsx
+++ app/page.tsx
@@ -7,33 +7,84 @@ export default function Home() {
   const [name, setName] = useState<string>("");
   const [nickname, setNickname] = useState<string>("");
   const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const createUser = async () => {
+    try {
+      const addingUser = await fetch("http://localhost:4444/api/users/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name,
+          nickname,
+          email
+        })
+      });
+      const data = await addingUser.json();
+      console.log("Utilisateur créé :", data);
+
+      setName("");
+      setNickname("");
+      setEmail("");
+    } catch (error) {
+      console.error("Erreur lors de la création de l'utilisateur :", error);
+    }
+  };
+
   return (
     <main className="grid grid-rows-[110px_1fr_70px] items-center justify-items-center min-h-screen ">
       <section className="bg-slate-800 w-full  flex items-center justify-center row-start-1 h-full">
-        <p> header</p>
+        <p> The perfumer's workbook</p>
       </section>
-      <section className="grid grid-cols-3 grid-rows-6 w-full bg-blue-200  row-start-2 h-full">
-        <div className="flex flex-col bg-white row-start-2 col-start-2 row-end-5 text-black shadow rounded-3xl items-center justify-between">
+      <section className="grid grid-cols-3 grid-rows-5 w-full bg-blue-200  row-start-2 h-full">
+        <div className="grid grid-rows-[auto_auto_auto_auto_auto_1fr] gap-4 bg-white row-start-2 col-start-2 row-end-4 text-black shadow rounded-3xl items-center justify-items-center">
           <h1 className="pb-3 pt-10">Bienvenue !</h1>
+
+          <input
+            placeholder="Email"
+            type="email"
+            name="email"
+            value={email}
+            required
+            onChange={(e): void => setEmail(e.target.value)}
+            className="border-2 rounded-3xl p-3 border-neutral-300 mt-1 w-[80%]"
+          ></input>
           <div className="flex flex-row gap-2 w-[80%] items-center justify-between">
             <input
-              placeholder="name"
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={name}
+              required
+              onChange={(e): void => setName(e.target.value)}
               className="border-2 rounded-3xl p-3 border-neutral-300 w-[50%]"
             ></input>
             <input
-              placeholder="nickname"
-              className="border-2 rounded-3xl p-3 border-neutral-300 w-[50%]"
+              placeholder="Nickname"
+              name="nickname"
+              value={nickname}
+              type="text"
+              required
+              onChange={(e): void => setNickname(e.target.value)}
+              className="border-1 rounded-3xl p-3 border-neutral-400 w-[50%] focus:placeholder-neutral-600 focus:border-purple-500 focus:outline-none placeholder-blue-800
+              "
             ></input>
           </div>
           <input
-            placeholder="email"
+            placeholder="Password"
+            type="password"
+            name="password"
+            value={password}
+            required
+            onChange={(e): void => setPassword(e.target.value)}
             className="border-2 rounded-3xl p-3 border-neutral-300 mt-1 w-[80%]"
           ></input>
-          <div className="grid grid-cols-2 w-full row-start-2 h-16">
+          <div className="grid grid-cols-2 w-full row-start-6 h-16">
             <button
-              onClick={(): void =>
-                console.log("L'utilisateur veut s'inscrire.")
-              }
+              type="submit"
+              onClick={createUser}
               className="flex col-start-1 bg-blue-500 w-full justify-center items-center rounded-bl-3xl font-bold text-white "
             >
               S'inscrire
